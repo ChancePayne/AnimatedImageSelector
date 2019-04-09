a557e4f01370765289778adb2297c9893d2a41eb
Chance Payne <chance@lambdaschool.com>
2019-04-09 11:10:26 -0600

    allow user to select resources and start animation. Add more resources

--- app/src/main/java/com/lambdaschool/animationselector/MainActivity.java
+++ app/src/main/java/com/lambdaschool/animationselector/MainActivity.java
@@ -2,13 +2,18 @@ package com.lambdaschool.animationselector;
 
 import android.graphics.drawable.AnimatedImageDrawable;
 import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
+    Spinner   resourceSelector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +21,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.image_view);
-//        ((AnimatedImageDrawable)imageView.getDrawable()).start();
-        imageView.setImageDrawable(getDrawable(R.drawable.walking_duck));
-        ((AnimationDrawable)imageView.getDrawable()).start();
+
+        resourceSelector = findViewById(R.id.image_selector);
+        resourceSelector.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String         selectedText = resourceSelector.getSelectedItem().toString();
+                final String[] stringArray  = getResources().getStringArray(R.array.resource_list);
+
+                if (selectedText.equals(stringArray[0])) {
+                    imageView.setImageDrawable(getDrawable(R.drawable.hourglass));
+                } else if (selectedText.equals(stringArray[1])) {
+                    imageView.setImageDrawable(getDrawable(R.drawable.walking_duck));
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        findViewById(R.id.start_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Drawable drawable = imageView.getDrawable();
+                if(drawable instanceof AnimatedImageDrawable) {
+                    ((AnimatedImageDrawable) drawable).start();
+                } else if (drawable instanceof AnimationDrawable) {
+                    ((AnimationDrawable) drawable).start();
+                }
+            }
+        });
+
+
     }
 }
