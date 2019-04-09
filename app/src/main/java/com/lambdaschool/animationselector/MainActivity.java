421ac129ef8cc859d14486508a05e1965d2ab5f3
Chance Payne <chance@lambdaschool.com>
2019-04-09 16:18:20 -0600

    fix logic of play/pause button

--- app/src/main/java/com/lambdaschool/animationselector/MainActivity.java
+++ app/src/main/java/com/lambdaschool/animationselector/MainActivity.java
@@ -1,6 +1,7 @@
 package com.lambdaschool.animationselector;
 
 import android.graphics.drawable.AnimatedImageDrawable;
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
     Spinner   resourceSelector;
+    boolean   isPlaying;
+    ImageView buttonView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +24,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.image_view);
+        isPlaying = false;
 
         resourceSelector = findViewById(R.id.image_selector);
         resourceSelector.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String         selectedText = resourceSelector.getSelectedItem().toString();
-                final String[] stringArray  = getResources().getStringArray(R.array.resource_list);
+                if(isPlaying) {
+                    setPlayButton(isPlaying);
+                    isPlaying = false;
+                }
 
-                if (selectedText.equals(stringArray[0])) {
-                    imageView.setImageDrawable(getDrawable(R.drawable.hourglass));
-                } else if (selectedText.equals(stringArray[1])) {
-                    imageView.setImageDrawable(getDrawable(R.drawable.walking_duck));
+                switch (position) {
+                    case 0:
+                        imageView.setImageDrawable(getDrawable(R.drawable.hourglass));
+                        break;
+                    case 1:
+                        imageView.setImageDrawable(getDrawable(R.drawable.walking_duck));
+                        break;
+                    case 2:
+                        imageView.setImageDrawable(getDrawable(R.drawable.gatsby));
+                        break;
+                    case 3:
+                        imageView.setImageDrawable(getDrawable(R.drawable.toaster));
+                        break;
                 }
             }
 
@@ -42,18 +57,50 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.start_button).setOnClickListener(new View.OnClickListener() {
+        buttonView = findViewById(R.id.start_button);
+        buttonView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 final Drawable drawable = imageView.getDrawable();
-                if(drawable instanceof AnimatedImageDrawable) {
-                    ((AnimatedImageDrawable) drawable).start();
+                if (drawable instanceof AnimatedImageDrawable) {
+
+                    final AnimatedImageDrawable animatedImageDrawable = (AnimatedImageDrawable) drawable;
+
+                    if (animatedImageDrawable.isRunning()) {
+                        animatedImageDrawable.stop();
+                    } else {
+                        animatedImageDrawable.start();
+                    }
+
                 } else if (drawable instanceof AnimationDrawable) {
-                    ((AnimationDrawable) drawable).start();
+
+                    final AnimationDrawable animationDrawable = (AnimationDrawable) drawable;
+
+                    if (animationDrawable.isRunning()) {
+                        animationDrawable.stop();
+                    } else {
+                        animationDrawable.start();
+                    }
                 }
+
+                setPlayButton(isPlaying);
+                isPlaying = !isPlaying;
+
             }
         });
+    }
 
+    private void setPlayButton(boolean isPlaying) {
+        if (!isPlaying) {
+            buttonView.setImageDrawable(getDrawable(R.drawable.avd_play_to_pause));
+        } else {
+            buttonView.setImageDrawable(getDrawable(R.drawable.avd_pause_to_play));
+        }
 
+        final Drawable buttonDrawable = buttonView.getDrawable();
+        if (buttonDrawable instanceof AnimatedVectorDrawable) {
+            ((AnimatedVectorDrawable) buttonDrawable).start();
+        }
     }
 }
