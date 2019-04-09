6a5c21e4992b4f9a6b3bed530fa45de251a978e4
Chance Payne <chance@lambdaschool.com>
2019-04-09 10:29:34 -0600

    add resources and build layout

--- app/src/main/java/com/lambdaschool/animationselector/MainActivity.java
+++ app/src/main/java/com/lambdaschool/animationselector/MainActivity.java
@@ -1,13 +1,23 @@
 package com.lambdaschool.animationselector;
 
+import android.graphics.drawable.AnimatedImageDrawable;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.image_view);
+//        ((AnimatedImageDrawable)imageView.getDrawable()).start();
+        imageView.setImageDrawable(getDrawable(R.drawable.walking_duck));
+        ((AnimationDrawable)imageView.getDrawable()).start();
     }
 }
